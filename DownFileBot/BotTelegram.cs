27f8e75dd692fd76fe8b25eb7a9e6ce0fe0b2596
IGNATOV93 <ignatowja@gmail.com>
2023-05-28 00:55:18 +0300

    пока так и не сделал статус отправки в сам телеграм

--- DownFileBot/BotTelegram.cs
+++ DownFileBot/BotTelegram.cs
@@ -1,7 +1,6 @@
 ﻿using IniParser;
 using IniParser.Model;
 using System.Net;
-using System.Reflection.Metadata;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.InputFiles;
@@ -20,7 +19,7 @@ public abstract class BotTelegram
     public static string DownfileIfo = "";
 
 
-public static async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+    public static async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
     {
         var buttonAuthTorrserver = new KeyboardButton("Настройка torrserver");
         var keyButtonMain = new ReplyKeyboardMarkup(buttonAuthTorrserver); keyButtonMain.ResizeKeyboard = true;
@@ -35,14 +34,15 @@ public static async Task Update(ITelegramBotClient botClient, Update update, Can
         {
             ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
             var message = update.Message;
-            
+
 
             if (message?.Text != null)
             {
                 IdChats = message?.Chat?.Id.ToString();
                 IdMessages = message.MessageId;
-           
-                if (message.Text == "/start") {
+
+                if (message.Text == "/start")
+                {
                     await botClient.DeleteMessageAsync(IdChats, IdMessages);
                     await botClient.SendTextMessageAsync(IdChats, "Привет,пришли мне ссылку на файл который хотите скачать \r\n а я его скачаю (до 1.85 гб размером)", replyMarkup: keyButtonMain);
                     return;
@@ -54,10 +54,10 @@ public static async Task Update(ITelegramBotClient botClient, Update update, Can
                         "пример : ivanov&&&1933abcd", replyMarkup: keyButtonMain);
                     return;
                 }
-                if(message.Text.Contains("&&&"))
+                if (message.Text.Contains("&&&"))
                 {
                     var authTorArray = message.Text.Split("&&&");
-                    string authString = authTorArray[0]+":"+authTorArray[1];
+                    string authString = authTorArray[0] + ":" + authTorArray[1];
                     data["Profile0"][IdChats] = authString;
                     parser.ReadFile(path);
                     parser.WriteFile("settings.ini", data);
@@ -68,18 +68,19 @@ public static async Task Update(ITelegramBotClient botClient, Update update, Can
                 if (Methods.UrlIsValid(message.Text.ToString()))
                 {
 
-                    var messageDownFile =await botClient.SendTextMessageAsync(IdChats, "Файл загружается ...");
+                    var messageDownFile = await botClient.SendTextMessageAsync(IdChats, "Файл загружается на vps...");
                     Task<string> t1 = Methods.DownFile(message.Text.ToString(), IdChats);
 
                     Task t2 = SendTextMessageInLoopAsync();// Создаем асинхронный метод для отправки текстовых сообщений
                     await Task.WhenAll(t1, t2); // Выполняем задачи параллельно и ждем результат их завершения
                     async Task SendTextMessageInLoopAsync()
                     {
+                        await Task.Delay(1000);
                         while (!t1.IsCompleted) // Цикл будет выполняться, пока задача t1 не завершится
                         {
-                            await Task.Delay(TimeSpan.FromSeconds(3)); // Задержка на  секунду
-                            await botClient.EditMessageTextAsync(IdChats,messageDownFile.MessageId,DownfileIfo); // Отправляем текстовое сообщение
-                            
+                            await Task.Delay(TimeSpan.FromSeconds(2)); // Задержка на  секунду
+                            await botClient.EditMessageTextAsync(IdChats, messageDownFile.MessageId, DownfileIfo); // Отправляем текстовое сообщение
+
                         }
                     }
                     PathDownFile = t1.Result;
@@ -89,30 +90,10 @@ public static async Task Update(ITelegramBotClient botClient, Update update, Can
                     {
                         var stream = new FileStream(PathDownFile, FileMode.Open);
                         InputOnlineFile file = new InputOnlineFile(stream, PathDownFile);
-                     var messageDounwFileTelegram=await botClient.SendTextMessageAsync(IdChats, "Зазрузка в телеграм..");
+                        var messageDounwFileTelegram = await botClient.SendTextMessageAsync(IdChats, "Зазрузка в телеграм..");
 
-                       var downFileTelegram=await botClient.SendDocumentAsync(IdChats, file, replyMarkup: keyButtonMain);
-                        string fileId = downFileTelegram.Document.FileId;
-                        long bytesUploaded = 0;
-                        var fileInfo = new FileInfo(PathDownFile);
-                        long fileSize = fileInfo.Length;
-                        DateTime startTime = DateTime.Now;
-                        while (bytesUploaded < fileSize)
-                        {
-                            // Отслеживать статус загрузки файла
-                            var File = await botClient.GetFileAsync(fileId);
-                            bytesUploaded = (long)File.FileSize;
-                            TimeSpan elapsed = DateTime.Now - startTime;
-                            double speedInBytesPerSecond = bytesUploaded / elapsed.TotalSeconds;
-                            double speedInMegabytesPerSecond = speedInBytesPerSecond / (1024 * 1024);
+                        var downFileTelegram = await botClient.SendDocumentAsync(IdChats, file, replyMarkup: keyButtonMain);
 
-                            Console.WriteLine("{0} MB / {1} MB - Speed: {2:0.00} MB/s", bytesUploaded / (1024 * 1024), fileSize / (1024 * 1024), speedInMegabytesPerSecond);
-
-                            // Задержка перед следующей проверкой статуса загрузки
-                            await Task.Delay(1000);
-                        }
-
-                        Console.WriteLine("Upload complete!");
 
                         Console.WriteLine("Файл загружен в телеграм сервер");
                         System.IO.File.Delete(PathDownFile);
@@ -123,7 +104,7 @@ public static async Task Update(ITelegramBotClient botClient, Update update, Can
                     return;
                 }
                 await botClient.DeleteMessageAsync(IdChats, IdMessages);
-                await botClient.SendTextMessageAsync(IdChats, "Ссылка на файл не корректная", replyMarkup:KeyboarDeleteMessage);
+                await botClient.SendTextMessageAsync(IdChats, "Ссылка на файл не корректная", replyMarkup: KeyboarDeleteMessage);
                 return;
             }
             if (update?.CallbackQuery?.Data != null) //ОБРАБОТКА UPDATE (inline)
@@ -131,18 +112,18 @@ public static async Task Update(ITelegramBotClient botClient, Update update, Can
                 string textInline = update.CallbackQuery.Data.ToString();
                 IdChats = update.CallbackQuery.Message.Chat.Id.ToString();
                 IdMessages = Int32.Parse(update.CallbackQuery.Message.MessageId.ToString());
-                if(textInline== "deletemessages")
+                if (textInline == "deletemessages")
                 {
                     await botClient.DeleteMessageAsync(IdChats, IdMessages);
                     return;
-                }    
+                }
             }
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             Console.WriteLine(e.ToString());
-            await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Что то пошло не так..",replyMarkup:KeyboarDeleteMessage);
-           
+            await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Что то пошло не так..", replyMarkup: KeyboarDeleteMessage);
+
         }
     }
     public static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
